100fa71300ab45b04b8cb1cd82f1a2c9e4b0d0b8
Eric Clemmons <eric@smarterspam.com>
2019-05-03 19:42:05 -0700

    Move example to index.html

--- src/App.svelte
+++ src/App.svelte
@@ -26,25 +26,6 @@
 
 <svelte:window on:load={() => target = document.querySelector("button")} on:keydown={handleKeydown}/>
 
-<div id="root" class="bg-teal-lightest min-h-screen">
-	<main class="py-6">
-		<div class="bg-white mx-auto max-w-sm shadow-lg rounded-lg overflow-hidden">
-			<div class="sm:flex sm:items-center px-6 py-4">
-				<img class="block h-16 sm:h-24 rounded-full mx-auto mb-4 sm:mb-0 sm:mr-4 sm:ml-0" src="https://github.com/ericclemmons.png" alt="">
-				<div class="text-center sm:text-left sm:flex-grow">
-					<div class="mb-4">
-						<p class="text-xl leading-tight">Eric Clemmons</p>
-						<p class="text-sm leading-tight text-grey-dark">Developer at Starbucks Coffee Company.</p>
-					</div>
-					<div>
-						<button class="text-xs font-semibold rounded-full px-4 py-1 leading-normal bg-white border border-purple text-purple hover:bg-purple hover:text-white">Message</button>
-					</div>
-				</div>
-			</div>
-		</div>
-	</main>
-</div>
-
 {#if active}
 	{#if target}
 		<Inspector target={target} />
